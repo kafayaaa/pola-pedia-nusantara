75f57572ad7911de40fb52dc34e26739c3c553d5
Kafayaaa <rizqikafamuntaqa777@gmail.com>
2026-02-14 00:10:56 +0700

    chore: initialize commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const jakarta = Plus_Jakarta_Sans({
+  variable: "--font-jakarta",
   subsets: ["latin"],
 });
 
@@ -23,12 +18,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="en" className="scroll-smooth">
+      <body className={`${jakarta.variable} antialiased`}>{children}</body>
     </html>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="fixed top-0 left-0 right-0 z-40 w-full pt-5 pb-10 px-5 bg-linear-to-b from-brand-black/50 to-brand-black/0">
+      <div className="w-full max-w-7xl mx-auto flex justify-between items-center">
+        <div className="max-w-40">
+          <Image src="/ppn.webp" alt="Logo" width={40} height={40} />
+        </div>
+        <ul className="flex items-center gap-10 text-brand-white">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/services">Services</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
